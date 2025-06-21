7a90bbc300488ed066623acf82de3baa0d02aea1
luisarteagadev <l.angelarteaga713@gmail.com>
2025-06-20 19:01:39 -0500

    se modifico la escucha a 0.0.0.0

--- app.py
+++ app.py
@@ -80,4 +80,5 @@ def predict():
         return jsonify({"error": error_msg}), 500
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000)
+
